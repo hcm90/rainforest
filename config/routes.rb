6dc31ae6db21c1fcf8bc5aa90b95d61a113d2ac1
“hcm90” <heitor.moura@gmail.com>
2014-02-03 15:32:37 -0500

    Views configured + partials created for new and edit forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rainforest::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  
+  resources :products
+  # get "products/index"
+  # get "products/show"
+  # get "products/new"
+  # get "products/edit"
+  # # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
