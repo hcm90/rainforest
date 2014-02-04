4d605c30b4036e1ed637b5b53d0b3bf2c452220e
“hcm90” <heitor.moura@gmail.com>
2014-02-04 15:44:44 -0500

    Added flash notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   		session[:user_id] = user.id
   		redirect_to products_url, :notice => "Logged in!"
   	else
+  		flash.now[:alert] = "invalid username or password"
   		render "new"
   	end
   end
